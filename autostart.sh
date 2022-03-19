f48a242c25cb9f58b852d4de6a86141f9bd504e9
programmer1024r <reempro112@gmail.com>
2022-03-19 23:42:26 +0200

    added autostart script

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 feh --randomize --bg-scale ~/BGs &
 
 picom &
 
 setxkbmap -model pc104 -layout us,il -option grp:win_space_toggle &
 
-sxhkd -c ~/sxhkd/sxhkdrc &
+sxhkd -c ./sxhkdrc &
 
 exec dwm 
 
